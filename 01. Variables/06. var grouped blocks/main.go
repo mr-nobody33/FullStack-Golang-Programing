9dcf4c0220be0935aa54e25419be60fc59958246
Mr. Nobody <helderberserk@gmail.com>
2023-08-30 17:58:06 -0300

    6. var grouped blocks

---
+++ 01. Variables/06. var grouped blocks/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	//define a groups of variables
+
+	var (
+		firstName = "Kim"
+		num1      = 300
+		num2      = 20.65
+		married   = true
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(num1)
+	fmt.Println(num2)
+	fmt.Println(married)
+}
