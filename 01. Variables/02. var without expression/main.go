282209baae66f93d00a91996674797ae7094218e
Mr. Nobody <helderberserk@gmail.com>
2023-08-30 17:20:56 -0300

    Variable 02 and 03

---
+++ 01. Variables/02. var without expression/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Variables declared without Initialized ( Without Expression )
+	var variable1 int
+	var variable2 string
+	var variable3 float64
+
+	fmt.Printf("Variabel1 value is : %d\n", variable1)
+	fmt.Printf("Variabel2 value is : %s\n", variable2)
+	fmt.Printf("Variabel1 value is : %f\n", variable3)
+}
---
+++ 01. Variables/03. var multiple same type/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	//define multiple variables of the same type are declared and initialized in the single line
+	var variable1, variable2, variable3 int = 2, 454, 67
+
+	fmt.Printf("Variable1 value: %d\n", variable1)
+	fmt.Printf("Variable2 value: %d\n", variable2)
+	fmt.Printf("Variable3 value: %d\n", variable3)
+}
