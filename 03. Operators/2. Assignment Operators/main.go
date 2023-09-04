48c4ebfd59f77f43378da1280dc46f5445a8a792
Mr. Nobody <helderberserk@gmail.com>
2023-09-04 18:53:01 -0300

    3.2 - Assignment Operators

---
+++ 03. Operators/2. Assignment Operators/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Assignment Operators
+
+	var a int = 30
+	var b int = 50
+
+	// a = b
+	// a += b
+	// a -= b
+	// a *= b
+	// a /= b
+	a %= b
+
+	fmt.Println("A = ", a)
+}
